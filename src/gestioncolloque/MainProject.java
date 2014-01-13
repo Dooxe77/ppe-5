fbe8682157667f6d11f5f7d13caaa913925bc7ef
Dooxe77 <anthony.ruelle.77@gmail.com>
2014-01-13 15:24:40 +0100

    Menu principal

---
+++ src/gestioncolloque/MainProject.java
@@ -0,0 +1,9 @@
+package gestioncolloque;
+public class MainProject {
+   public static void main (String[] args){
+       MenuPrincipal fen = new MenuPrincipal();
+       fen.setLocationRelativeTo(null);
+       fen.setVisible(true);
+    }
+}
+
